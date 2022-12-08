5ccdab7890098c25b1cc4cf9557374abcda4723e
Sergey Khromin <s-hromin@mail.ru>
2022-12-09 05:42:34 +0700

    add backUps

--- lab3/Controls/GL/ImgBitmap.cs
+++ lab3/Controls/GL/ImgBitmap.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System;
+using System.Xml.Serialization;
+using Avalonia;
 
 namespace lab3.Controls.GL;
 
@@ -14,33 +16,39 @@ public class ImgBitmap
         RenderHeight = Height;
         _renderWidthToHeight = RenderWidth / RenderHeight;
     }
-    
+
+    public ImgBitmap()
+    {
+    }
+
+
     public int Width { get; }
-    
+
     public int Height { get; }
-    
+
     public byte[] Pixels { get; }
+
+    [field: NonSerialized] [XmlIgnore] public float RenderWidth { get; private set; }
     
-    public float RenderWidth { get; private set; }
-    public float RenderHeight { get; private set; }
+    [field: NonSerialized] [XmlIgnore] public float RenderHeight { get; private set; }
 
     private float _renderWidthToHeight;
-    
+
     public void OnRender(float boundsWidth, float boundsHeight)
     {
         RenderWidth = Width;
         RenderHeight = Height;
         _renderWidthToHeight = RenderWidth / RenderHeight;
-        
+
         RecalculateSize(boundsWidth, boundsHeight);
     }
-    
+
     private void RecalculateSize(float boundsWidth, float boundsHeight)
     {
         if (RenderWidth > RenderHeight)
         {
             RecalculateImageWidth(boundsWidth);
-    
+
             if (RenderHeight > boundsHeight)
             {
                 RecalculateImageHeight(boundsHeight);
@@ -48,21 +56,21 @@ public class ImgBitmap
 
             return;
         }
-    
+
         RecalculateImageHeight(boundsHeight);
-        
+
         if (RenderWidth > boundsWidth)
         {
             RecalculateImageWidth(boundsWidth);
         }
     }
-    
+
     private void RecalculateImageWidth(float boundsWidth)
     {
         RenderWidth = boundsWidth;
         RenderHeight = RenderWidth / _renderWidthToHeight;
     }
-    
+
     private void RecalculateImageHeight(float boundsHeight)
     {
         RenderHeight = boundsHeight;
